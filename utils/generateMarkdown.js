c3f094cc56da913a9c67dc476ed16b9014b21b10
Grigore <grigore.mereacre95@gmail.com>
2020-06-10 21:13:00 -0500

    generateMarkdown done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![badge](https://img.shields.io/badge/License-${ data.licences }-blue.svg)
 
+  ## Table of Contests
+  -[Description](#description)
+  -[Instalation](#install)
+  -[Usage](#usage)
+  -[Licences](#licences)
+  -[Contribution](#contribution)
+  -[Test](#tests)
+  -[Question](#questions)
+
+  ##Description
+  ### ${data.description}
+
+  ##Instal
+  ${data.install}
+
+  ##Usage
+  ### ${data.usage}
+
+  ##Licence
+  ${data.licences}
+
+  ##Contribution
+  ### ${data.contribution}
+
+  ##Test
+  ### ${data.tests}
+
+  ##Questions
+  ###### [${ data.username } for GitHub](https://github.com/)  
+  ### ${ data.email } for ✉️ email
 `;
 }
 
